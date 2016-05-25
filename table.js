2bda92c1526833bf3114161296b5236b1b70f1a2
Max Norton <maxbnorton@gmail.com>
2016-05-25 16:00:00 -0700

    troubleshoot yieldRates function

--- table.js
+++ table.js
@@ -1,43 +1,42 @@
 var t, a, eff; // indices
 var cost1, cost2, cost3, cost4, pc, price, yield1, yield2, yield3, yield4, yield5; // user-set parameters
 var cdnb, nr, yield; // outcomes
-var yieldRates = new Array();
+var yields;
 
 function the_table(cost1, cost2, cost3, cost4, pc, price, yield1, yield2, yield3, yield4, yield5) {
 	console.log(cost1, cost2, cost3, cost4, pc, price, yield1, yield2, yield3, yield4, yield5);
 	d3.tsv("yield-rates.tsv", function(data) {
 		for (i=1; i<data.length; i++) {
-			console.log(data[i]);
-			yieldRates[i-1] = data[i][3];
+			var j = i-1;
 		}
-	});
-	var yields = {
-		0 : yield1*yieldRates[0],
-		1 : yield1*yieldRates[1],
-		2 : yield1*yieldRates[2],
-		3 : yield1*yieldRates[3],
-		4 : yield1*yieldRates[4],
-		5 : yield1*yieldRates[5],
-		6 : yield1*yieldRates[6],
-		7 : yield1*yieldRates[7],
-		8 : yield1*yieldRates[8],
-		9 : yield1*yieldRates[9],
-		10 : yield1*yieldRates[10],
-		11 : yield1*yieldRates[11],
-		12 : yield1*yieldRates[12],
-		13 : yield1*yieldRates[13],
-		14 : yield1*yieldRates[14],
-		15 : yield1*yieldRates[15],
-		16 : yield1*yieldRates[16],
-		17 : yield1*yieldRates[17],
-		18 : yield1*yieldRates[18],
-		19 : yield1*yieldRates[19],
-		20 : yield1*yieldRates[20],
-		21 : yield1*yieldRates[21],
-		22 : yield1*yieldRates[22],
-		23 : yield1*yieldRates[23],
-		24 : yield1*yieldRates[24],
-		25 : yield1*yieldRates[25]
-	};
+		yields = {
+			0 : yield1*data[0]['25y03']/100,
+			1 : yield1*data[1]['25y03']/100,
+			2 : yield2*data[2]['25y03']/100,
+			3 : yield3*data[3]['25y03']/100,
+			4 : yield4*data[4]['25y03']/100,
+			5 : yield5*data[5]['25y03']/100,
+			6 : yield5*data[6]['25y03']/100,
+			7 : yield5*data[7]['25y03']/100,
+			8 : yield5*data[8]['25y03']/100,
+			9 : yield5*data[9]['25y03']/100,
+			10 : yield5*data[10]['25y03']/100,
+			11 : yield5*data[11]['25y03']/100,
+			12 : yield5*data[12]['25y03']/100,
+			13 : yield5*data[13]['25y03']/100,
+			14 : yield5*data[14]['25y03']/100,
+			15 : yield5*data[15]['25y03']/100,
+			16 : yield5*data[16]['25y03']/100,
+			17 : yield5*data[17]['25y03']/100,
+			18 : yield5*data[18]['25y03']/100,
+			19 : yield5*data[19]['25y03']/100,
+			20 : yield5*data[20]['25y03']/100,
+			21 : yield5*data[21]['25y03']/100,
+			22 : yield5*data[22]['25y03']/100,
+			23 : yield5*data[23]['25y03']/100,
+			24 : yield5*data[24]['25y03']/100,
+			25 : yield5*data[25]['25y03']/100
+		};
 	console.log(yields);
+	});
 };
