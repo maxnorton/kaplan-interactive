760867df13dc1e3290cfd979b575078c66527f60
Max Norton <maxbnorton@gmail.com>
2016-05-30 17:20:39 -0700

    rewrite array constructs as for loops

--- table.js
+++ table.js
@@ -76,7 +76,6 @@ function the_table(age, efficacy, discount, cost0, cost1, cost2, cost3, pc, pric
 		];
 
 		var pcFtnOfT = [];
-
 		for (var l=0; l<age; l++) {
 			pcFtnOfT[l] = 0;
 		};
@@ -84,66 +83,18 @@ function the_table(age, efficacy, discount, cost0, cost1, cost2, cost3, pc, pric
 			pcFtnOfT[l] = pc;
 		};
 
-		treatedNR = [
-			price*treatedYields[0]-costs[0]-pcFtnOfT[0],
-			price*treatedYields[1]-costs[1]-pcFtnOfT[1],
-			price*treatedYields[2]-costs[2]-pcFtnOfT[2],
-			price*treatedYields[3]-costs[3]-pcFtnOfT[3],
-			price*treatedYields[4]-costs[4]-pcFtnOfT[4],
-			price*treatedYields[5]-costs[5]-pcFtnOfT[5],
-			price*treatedYields[6]-costs[6]-pcFtnOfT[6],
-			price*treatedYields[7]-costs[7]-pcFtnOfT[7],
-			price*treatedYields[8]-costs[8]-pcFtnOfT[8],
-			price*treatedYields[9]-costs[9]-pcFtnOfT[9],
-			price*treatedYields[10]-costs[10]-pcFtnOfT[10],
-			price*treatedYields[11]-costs[11]-pcFtnOfT[11],
-			price*treatedYields[12]-costs[12]-pcFtnOfT[12],
-			price*treatedYields[13]-costs[13]-pcFtnOfT[13],
-			price*treatedYields[14]-costs[14]-pcFtnOfT[14],
-			price*treatedYields[15]-costs[15]-pcFtnOfT[15],
-			price*treatedYields[16]-costs[16]-pcFtnOfT[16],
-			price*treatedYields[17]-costs[17]-pcFtnOfT[17],
-			price*treatedYields[18]-costs[18]-pcFtnOfT[18],
-			price*treatedYields[19]-costs[19]-pcFtnOfT[19],
-			price*treatedYields[20]-costs[20]-pcFtnOfT[20],
-			price*treatedYields[21]-costs[21]-pcFtnOfT[21],
-			price*treatedYields[22]-costs[22]-pcFtnOfT[22],
-			price*treatedYields[23]-costs[23]-pcFtnOfT[23],
-			price*treatedYields[24]-costs[24]-pcFtnOfT[24],
-			price*treatedYields[25]-costs[25]-pcFtnOfT[25]
-		];
+		var treatedNR = [];
+		for (var i in treatedYields) {
+			treatedNR[i] = price*treatedYields[i]-costs[i]-pcFtnOfT[i];
+		};
 
-		treatedDNR = [
-			treatedNR[0]*(1/(1+discount/100)),
-			treatedNR[1]*(1/(1+discount/100)),
-			treatedNR[2]*(1/(1+discount/100)),
-			treatedNR[3]*(1/(1+discount/100)),
-			treatedNR[4]*(1/(1+discount/100)),
-			treatedNR[5]*(1/(1+discount/100)),
-			treatedNR[6]*(1/(1+discount/100)),
-			treatedNR[7]*(1/(1+discount/100)),
-			treatedNR[8]*(1/(1+discount/100)),
-			treatedNR[9]*(1/(1+discount/100)),
-			treatedNR[10]*(1/(1+discount/100)),
-			treatedNR[11]*(1/(1+discount/100)),
-			treatedNR[12]*(1/(1+discount/100)),
-			treatedNR[13]*(1/(1+discount/100)),
-			treatedNR[14]*(1/(1+discount/100)),
-			treatedNR[15]*(1/(1+discount/100)),
-			treatedNR[16]*(1/(1+discount/100)),
-			treatedNR[17]*(1/(1+discount/100)),
-			treatedNR[18]*(1/(1+discount/100)),
-			treatedNR[19]*(1/(1+discount/100)),
-			treatedNR[20]*(1/(1+discount/100)),
-			treatedNR[21]*(1/(1+discount/100)),
-			treatedNR[22]*(1/(1+discount/100)),
-			treatedNR[23]*(1/(1+discount/100)),
-			treatedNR[24]*(1/(1+discount/100)),
-			treatedNR[25]*(1/(1+discount/100))
-		];
+		var treatedDNR = [];
+		for (var i in treatedNR) {
+			treatedDNR[i] = treatedNR*(1/(1+discount/100));
+		};
 
 		treatedCDNR = [ treatedDNR[0] ];
-		for (var m=1; m<26; m++) {
+		for (var in in treatedDNR) {
 			treatedCDNR[m] = treatedDNR[m] + treatedCDNR[m-1];
 		};
 
